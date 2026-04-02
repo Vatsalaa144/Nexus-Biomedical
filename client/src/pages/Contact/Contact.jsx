a4d77d5c74cc4a77c7197ceee9c20fc9dac802d8
Vatsalaa144 <vatsalashukla7@gmail.com>
2026-04-02 14:20:51 +0530

    fixed

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
     try {
       const API_BASE = import.meta.env.VITE_API_URL; // or VITE_API_URL if you used that name
 
-      const response = await fetch(`${API_BASE}/api/contact`, {
+      const response = await fetch(`${API_BASE}/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
